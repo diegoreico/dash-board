fbf9206e460b9a3696c9b9343d6fe8c9895bf9c1
Diego Reiriz Cores <diegoreiriz@gmail.com>
2019-05-14 17:57:40 +0200

    Added SGD class

---
+++ src/services/RecommendationService.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+class SGD:
+
+    def __init__(self, data: np.ndarray, n_factors: int, alpha: int, n_epochs:int) -> None:
+        super().__init__()
+        self.data = data
+        self.n_factors = n_factors
+        self.alpha = alpha
+        self.n_epochs = n_epochs
+
+        self._u: np.ndarray = None
+        self._v: np.ndarray = None
+
+    def train(self):
+        '''Learn the vectors p_u and q_i with SGD.
+           data is the user-item matrix
+           n_factor is the number of latent factors to use
+           alppha is the learning rate of the SGD
+           n_epochs is the number of iterations to run the algorithm
+        '''
+        shape = np.shape(self.data)
+        n_users = shape[0]
+        n_items = shape[1]
+
+        # Randomly initialize the user and item factors.
+        p = np.random.normal(0, .1, (n_users, self.n_factors))
+        q = np.random.normal(0, .1, (n_items, self.n_factors))
+
+        # Optimization procedure
+        for _ in range(self.n_epochs):
+            for (u, i), r_ui in np.ndenumerate(self.data):
+                if r_ui > 0:
+                    err = r_ui - np.dot(p[u], q[i])
+                    # Update vectors p_u and q_i
+                    p[u] += self.alpha * err * q[i]
+                    q[i] += self.alpha * err * p[u]
+
+        self._u = p
+        self._v = q
+        return p, q
+
+    def rmse(self, u: np.ndarray, v: np.ndarray) -> int:
+        errors = u - v
+        return np.sqrt(np.sum(errors * errors) / errors.size)
+
